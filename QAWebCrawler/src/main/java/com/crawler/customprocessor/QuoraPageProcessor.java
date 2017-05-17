5db88f9ebd2fef3cc785235b6b19028aa1468cee
Shi Xiyue <shixiyue97@gmail.com>
2017-05-17 15:11:40 +0800

    Write documentation for QuoraPageProcessor

--- QAWebCrawler/src/main/java/com/crawler/customprocessor/QuoraPageProcessor.java
+++ QAWebCrawler/src/main/java/com/crawler/customprocessor/QuoraPageProcessor.java
@@ -21,29 +21,39 @@ import javax.management.JMException;
 
 import net.htmlparser.jericho.Source;
 
+/**
+ * Represents a page processor that can process web pages of Quora.
+ * It uses the crawler framework WebMagic.
+ */
 public class QuoraPageProcessor implements PageProcessor {
 
+	// Crawler configuration
 	private Site site = Site.me().setCycleRetryTimes(5).setRetryTimes(5).setSleepTime(500).setTimeOut(5 * 60 * 1000)
 			.setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0")
 			.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 			.addHeader("Accept-Language", "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3").setCharset("UTF-8");
 
 	/**
-	 *
-	 * @param page
+	 * Processes the page.
 	 */
 	@Override
 	public void process(Page page) {
-		addRelativeUrls(page);
+		addRelatedQuestionsUrls(page);
 		extractContent(page);
 	}
 
-	private void addRelativeUrls(Page page) {
-		List<String> relativeUrl = page.getHtml()
+	/**
+	 * Finds urls from the Quora related question field and adds them to the queue to be crawled.
+	 */
+	private void addRelatedQuestionsUrls(Page page) {
+		List<String> relatedUrl = page.getHtml()
 				.xpath("//li[@class='related_question']//a[@class='question_link']/@href").all();
-		page.addTargetRequests(relativeUrl);
+		page.addTargetRequests(relatedUrl);
 	}
 
+	/**
+	 * Extracts useful content (url, question, description, topics and answer list) from the page.
+	 */
 	private void extractContent(Page page) {
 		String url = page.getUrl().toString();
 		String question = page.getHtml().xpath("//h1//span[@class='rendered_qtext']/text()").toString();
@@ -65,6 +75,10 @@ public class QuoraPageProcessor implements PageProcessor {
 		}
 	}
 
+	/**
+	 * Gets answers and their votes from the web page.
+	 * @return ArrayList<HashMap<String, Object>> answerList
+	 */
 	private ArrayList<HashMap<String, Object>> getAnswerList(Page page) {
 		List<String> answers = page.getHtml()
 				.xpath("//div[@class='pagedlist_item']//div[@class='ExpandedQText ExpandedAnswer']/span").all();
@@ -75,27 +89,43 @@ public class QuoraPageProcessor implements PageProcessor {
 		for (int i = 0; i < answers.size(); i++) {
 			String votesText = new Html(votes.get(i))
 					.xpath("//a[@class='AnswerVoterListModalLink VoterListModalLink']/text()").toString();
-			if (votesText == null) {
+			int vote;
+			if (votesText == null) { // i.e the answer does not have any vote
+				// then we consider the answer not useful and don't store it. (There's no negative vote in Quora)
 				continue;
 			} else {
-				votesText = votesText.split(" ")[0].replaceAll(",", "");
+				vote = formatVote(votesText);
 			}
 			String answerText = extractAllText(
 					new Html(answers.get(i)).xpath("//span[@class='rendered_qtext']").toString());
 
 			HashMap<String, Object> answer = new HashMap<String, Object>();
-			answer.put(Config.VOTE, Integer.parseInt(votesText));
+			answer.put(Config.VOTE, vote);
 			answer.put(Config.ANSWER, answerText);
 			answerList.add(answer);
 		}
 		return answerList;
 	}
 
+	/**
+	 * Parses votesText, which is in the format "XX,XXX Upvotes".
+	 * @return Integer an integer representation of the number of votes
+	 */
+	private Integer formatVote(String votesText) {
+		return Integer.parseInt(votesText.split(" ")[0].replaceAll(",", ""));
+	}
+
+	/**
+	 * Returns true when the page is not useful and should be skipped, i.e. the question is empty or the list of useful answers is empty.
+	 */
 	private boolean shouldSkip(String question, ArrayList<HashMap<String, Object>> answerList) {
 		return question.isEmpty() || answerList.isEmpty();
 	}
 
-	public String extractAllText(String htmlText) {
+	/**
+	 * Extracts all text from the given html text
+	 */
+	private String extractAllText(String htmlText) {
 		Source source = new Source(htmlText);
 		return source.getTextExtractor().toString();
 	}
@@ -106,20 +136,26 @@ public class QuoraPageProcessor implements PageProcessor {
 	}
 
 	/**
-	 * This Spider starts from a random link and crawl the page through the
-	 * quora related question field. For next step, a key word based crawler.
-	 * You can use： scheduler.push(new
-	 * Request("www.quora.com/topic/arg0-arg1-arg2-arg3")) where args form the
-	 * keyword phrase
+	 * The Spider starts from links given in
+	 * "src\main\resources\filecachepath\www.quora.com.urls.txt" and find more
+	 * pages to crawl through the Quora related question field. New links will
+	 * be added to the txt file. Those pages will be downloaded, processed and
+	 * output as json files.
+	 * 
+	 * Note: "www.quora.com.urls - Copy.txt" contains links of 150 popular
+	 * questions that are evenly distributed in 30 most popular topics (i.e. we
+	 * select 5 questions from each topic), based on the list in
+	 * https://www.quora.com/What-are-the-most-followed-topics-on-Quora-2 Those
+	 * links are chosen for initial links.
 	 *
 	 * @param args
 	 * @throws JMException
 	 */
 	public static void main(String[] args) throws JMException {
-
 		final String bloomObjPath = "src/main/resources/bloompath/bloom.ser";
 		String fileCachePath = "src/main/resources/filecachepath/";
 		String seleniumPath = "src/main/resources/chromedriver";
+		// A dummy placeholder URL.
 		String url = "https://www.quora.com/I-wanna-study-hard-but-I-cant-how-can-I-motivate-myself-for-that";
 		int numOfExpectedData = 50000000;
 		double falseRate = 0.01;
@@ -134,6 +170,8 @@ public class QuoraPageProcessor implements PageProcessor {
 			System.out.println("Bloom Filter Object does not exist, preparing a new one...");
 		}
 
+		// Docs:
+		// http://webmagic.io/docs/en/posts/ch6-custom-componenet/pipeline.html
 		Spider quoraSpider = Spider.create(new QuoraPageProcessor()).addUrl(url)
 				.addPipeline(new JsonFilePipeline("/crawlResult")).setDownloader(new SeleniumDownloader(seleniumPath))
 				.thread(10).setScheduler(new FileCacheQueueScheduler(fileCachePath).setDuplicateRemover(pbf));
--- QAWebCrawler/src/main/java/com/crawler/customutil/Config.java
+++ QAWebCrawler/src/main/java/com/crawler/customutil/Config.java
@@ -6,13 +6,9 @@ public class Config {
 	public final static String ANSWER = "answer";
 	public final static String VOTE = "vote";
 	public final static String URL = "url";
-	public final static String CATEGORIES = "categories";
 	public final static String QUESTION = "question";
 	public final static String DESCRIPTION = "description";
 	public final static String ANSWERS = "answers";
 	public final static String TOPICS = "topics";
-	
-	// Constants
-	public final static String stringDelimiter = " ";
 
 }
